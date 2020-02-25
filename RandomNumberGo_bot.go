505d9fc714bc36156cf1938ef2c51ad35a8201f8
Despenrado <StepNikdesp@gmail.com>
2020-02-25 20:07:37 +0100

    created help message created /start message renamed modst into templates add log of count of templates are loaded from resources.xml

--- RandomNumberGo_bot.go
+++ RandomNumberGo_bot.go
@@ -14,20 +14,32 @@ import (
 )
 
 var (
-	modesMap map[int]*Mode
-	help     = "help"
-	mods     Modes
+	templatesMap map[int]*Template
+	help         = "help message:\n" +
+		"structure of queries: /command [parametr] [parametr] ....\n" +
+		"example: /setmin 10 - this command sets minimum border to 10\n\n" +
+		"commands:\n" +
+		"help - shows this message\n" +
+		"settemplate - shows list of templates\n" +
+		"setmin [number]- sets minimum border\n" +
+		"setmax [number] - sets maximum border\n" +
+		"setquantity [number] - sets number of random numbers\n" +
+		"setminmaxqua [min] [max] [quantity] - sets minimum, maximum and quantity\n" +
+		"setwords [word1;word2;word3] - sets words for random choice\n" +
+		"status - shows your current tamplate of rundom\n" +
+		"roll - genertes random nuber/numbers via current template"
+	templates Templates
 )
 
 type Config struct {
 	TelegramBotToken string
 }
 
-type Modes struct {
-	Mode []Mode `xml:"Mode"`
+type Templates struct {
+	Template []Template `xml:"Template"`
 }
 
-type Mode struct {
+type Template struct {
 	Name     string   `xml:"Name"`
 	Min      int      `xml:"Min"`
 	Max      int      `xml:"Max"`
@@ -36,8 +48,8 @@ type Mode struct {
 }
 
 func main() {
-	modesMap = make(map[int]*Mode)
-	mods, _ = parseModes()
+	templatesMap = make(map[int]*Template)
+	templates, _ = parseTemplates()
 
 	config, err := parceConfig()
 	if err != nil {
@@ -68,10 +80,10 @@ func main() {
 			case "roll":
 				roll(userID, chatID, query, bot)
 			default:
-				for _, mode := range mods.Mode {
-					if query == mode.Name {
-						tmp := mode
-						modesMap[userID] = &tmp
+				for _, template := range templates.Template {
+					if query == template.Name {
+						tmp := template
+						templatesMap[userID] = &tmp
 						break
 					}
 				}
@@ -80,6 +92,7 @@ func main() {
 			}
 		}
 		if update.Message != nil {
+
 			userID := update.Message.From.ID
 			chatID := update.Message.Chat.ID
 			query := update.Message.Text
@@ -88,27 +101,31 @@ func main() {
 			command = update.Message.Command()
 			if command == "" {
 				//for text
-				bot.Send(tgbotapi.NewMessage(chatID, help))
+				bot.Send(tgbotapi.NewMessage(chatID, "/help shows help message"))
 			} else {
 				//for commands
 				switch command {
-				case "setmode":
-					msg := tgbotapi.NewMessage(chatID, "Select mode")
+				case "start":
+					msg := tgbotapi.NewMessage(chatID, "Welcome to RandomNumber_bot\n"+
+						"/help shows help message")
+					bot.Send(msg)
+				case "settemplate":
+					msg := tgbotapi.NewMessage(chatID, "Select template")
 					buttons := tgbotapi.InlineKeyboardMarkup{}
-					for _, mode := range mods.Mode {
+					for _, template := range templates.Template {
+						log.Println("\n22222222222222222222222222222222222222222222222222222222222222222222222222222222222222\n")
 						var row []tgbotapi.InlineKeyboardButton
-						btn := tgbotapi.NewInlineKeyboardButtonData(mode.Name, mode.Name)
+						btn := tgbotapi.NewInlineKeyboardButtonData(template.Name, template.Name)
 						row = append(row, btn)
 						buttons.InlineKeyboard = append(buttons.InlineKeyboard, row)
 					}
-					status(userID, chatID, query, bot)
 					msg.ReplyMarkup = buttons
 					bot.Send(msg)
 				case "setquantity":
-					if _, ok := modesMap[userID]; ok {
+					if _, ok := templatesMap[userID]; ok {
 						commands := strings.Split(query, " ")
 						if len(commands) > 1 {
-							modesMap[userID].Quantity, err = strconv.Atoi(commands[1])
+							templatesMap[userID].Quantity, err = strconv.Atoi(commands[1])
 							if err != nil {
 								msg := tgbotapi.NewMessage(chatID, "wrong format")
 								bot.Send(msg)
@@ -119,14 +136,14 @@ func main() {
 							bot.Send(msg)
 						}
 					} else {
-						msg := tgbotapi.NewMessage(chatID, "please use /setmode before using "+query)
+						msg := tgbotapi.NewMessage(chatID, "please use /settemplate before using "+query)
 						bot.Send(msg)
 					}
 				case "setmin":
-					if _, ok := modesMap[userID]; ok {
+					if _, ok := templatesMap[userID]; ok {
 						commands := strings.Split(query, " ")
 						if len(commands) > 1 {
-							modesMap[userID].Min, err = strconv.Atoi(commands[1])
+							templatesMap[userID].Min, err = strconv.Atoi(commands[1])
 							if err != nil {
 								msg := tgbotapi.NewMessage(chatID, "wrong format")
 								bot.Send(msg)
@@ -137,14 +154,14 @@ func main() {
 							bot.Send(msg)
 						}
 					} else {
-						msg := tgbotapi.NewMessage(chatID, "please use /setmode before using "+query)
+						msg := tgbotapi.NewMessage(chatID, "please use /settemplate before using "+query)
 						bot.Send(msg)
 					}
 				case "setmax":
-					if _, ok := modesMap[userID]; ok {
+					if _, ok := templatesMap[userID]; ok {
 						commands := strings.Split(query, " ")
 						if len(commands) > 1 {
-							modesMap[userID].Max, err = strconv.Atoi(commands[1])
+							templatesMap[userID].Max, err = strconv.Atoi(commands[1])
 							if err != nil {
 								msg := tgbotapi.NewMessage(chatID, "wrong format")
 								bot.Send(msg)
@@ -155,29 +172,28 @@ func main() {
 							bot.Send(msg)
 						}
 					} else {
-						msg := tgbotapi.NewMessage(chatID, "please use /setmode before using "+query)
+						msg := tgbotapi.NewMessage(chatID, "please use /settemplate before using "+query)
 						bot.Send(msg)
 					}
 				case "setwords":
-					if _, ok := modesMap[userID]; ok {
+					if _, ok := templatesMap[userID]; ok {
 						commands := strings.Split(query, "/setwords ")
-						log.Println("==================================================" + commands[1])
 						words := strings.Split(commands[1], ";")
 						for _, word := range words {
-							modesMap[userID].Values = append(modesMap[userID].Values, word)
+							templatesMap[userID].Values = append(templatesMap[userID].Values, word)
 						}
 						status(userID, chatID, query, bot)
 					} else {
-						msg := tgbotapi.NewMessage(chatID, "please use /setmode before using "+query)
+						msg := tgbotapi.NewMessage(chatID, "please use /settemplate before using "+query)
 						bot.Send(msg)
 					}
 				case "setminmaxqua":
-					if _, ok := modesMap[userID]; ok {
+					if _, ok := templatesMap[userID]; ok {
 						commands := strings.Split(query, " ")
 						if len(commands) > 3 {
-							modesMap[userID].Min, err = strconv.Atoi(commands[1])
-							modesMap[userID].Max, err = strconv.Atoi(commands[2])
-							modesMap[userID].Quantity, err = strconv.Atoi(commands[3])
+							templatesMap[userID].Min, err = strconv.Atoi(commands[1])
+							templatesMap[userID].Max, err = strconv.Atoi(commands[2])
+							templatesMap[userID].Quantity, err = strconv.Atoi(commands[3])
 							if err != nil {
 								msg := tgbotapi.NewMessage(chatID, "wrong format")
 								bot.Send(msg)
@@ -188,7 +204,7 @@ func main() {
 							bot.Send(msg)
 						}
 					} else {
-						msg := tgbotapi.NewMessage(chatID, "please use /setmode before using "+query)
+						msg := tgbotapi.NewMessage(chatID, "please use /settemplate before using "+query)
 						bot.Send(msg)
 					}
 				case "status":
@@ -206,19 +222,19 @@ func main() {
 }
 
 func status(userID int, chatID int64, query string, bot *tgbotapi.BotAPI) {
-	if val, ok := modesMap[userID]; ok {
-		msg := tgbotapi.NewMessage(chatID, "Your \"random\":\nMode: "+val.Name+"\nQuantity: "+strconv.Itoa(int(val.Quantity))+"\nMin: "+
+	if val, ok := templatesMap[userID]; ok {
+		msg := tgbotapi.NewMessage(chatID, "Your \"random\":\nTemplate: "+val.Name+"\nQuantity: "+strconv.Itoa(int(val.Quantity))+"\nMin: "+
 			strconv.Itoa(val.Min)+"\nMax: "+strconv.Itoa(val.Max)+"\nWords: "+strings.Join(val.Values, ";")+
 			"\n\n/help for help\n/roll for roll")
 		bot.Send(msg)
 	} else {
-		msg := tgbotapi.NewMessage(chatID, "Mode not found. Please use /setmode before using "+query)
+		msg := tgbotapi.NewMessage(chatID, "Template not found. Please use /settemplate before using "+query)
 		bot.Send(msg)
 	}
 }
 
 func roll(userID int, chatID int64, query string, bot *tgbotapi.BotAPI) {
-	if val, ok := modesMap[userID]; ok {
+	if val, ok := templatesMap[userID]; ok {
 		if val.Max > val.Min || len(val.Values) > 0 {
 			sum := 0
 			msgText := "\n"
@@ -232,7 +248,7 @@ func roll(userID int, chatID int64, query string, bot *tgbotapi.BotAPI) {
 					msgText += val.Values[tmp] + "\n"
 				}
 			}
-			msgText += "sum= " + strconv.Itoa(sum)
+			msgText += "sum= " + strconv.Itoa(sum) + "\navg= " + strconv.FormatFloat(float64(sum)/float64(val.Quantity), 'f', -4, 32)
 			msg := tgbotapi.NewMessage(chatID, msgText)
 			bot.Send(msg)
 		} else {
@@ -240,7 +256,7 @@ func roll(userID int, chatID int64, query string, bot *tgbotapi.BotAPI) {
 			bot.Send(msg)
 		}
 	} else {
-		msg := tgbotapi.NewMessage(chatID, "please use /setmode before using "+query)
+		msg := tgbotapi.NewMessage(chatID, "please use /settemplate before using "+query)
 		bot.Send(msg)
 	}
 }
@@ -252,16 +268,20 @@ func randomNumber(num1 string, num2 string) (string, error) {
 	return msg, err
 }
 
-func parseModes() (Modes, error) {
+func parseTemplates() (Templates, error) {
 	file, err := os.Open("resources.xml")
 	if err != nil {
 		log.Panic(err)
 	}
 	defer file.Close()
 	byteValue, err := ioutil.ReadAll(file)
-	var tmpModes Modes
-	xml.Unmarshal(byteValue, &tmpModes)
-	return tmpModes, err
+	var tmpTemplates Templates
+	err = xml.Unmarshal(byteValue, &tmpTemplates)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("length of templates slice=" + strconv.Itoa(len(tmpTemplates.Template)))
+	return tmpTemplates, err
 }
 
 func parceConfig() (Config, error) {
